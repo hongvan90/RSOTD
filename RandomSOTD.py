99b171ef21f78050d27b99ba1874d97ca8475903
GildaGriffon <zxnoregretzxz@hotmail.co.uk>
2015-08-31 21:47:29 +0100

    Added the actuall script + a frame for save to file
    Just a small edit. If anyone knows how to save it to the actual file
    make a pull request

---
+++ RandomSOTD.py
@@ -0,0 +1,13 @@
+import praw, pprint, pickle
+user_agent = "Gilda" ##input("Please input your desired user agent to display to reddit: ")
+r = praw.Reddit(user_agent=user_agent)
+nsfwCheck = input("Allow NSFW subs to be displayed?(True or False): ")
+subreddit = r.get_random_subreddit(nsfw=nsfwCheck)
+storeIt = pprint.pprint(subreddit)
+strSub = str(subreddit)
+storeIt
+outfile = open('RSOTD.txt','w')
+outfile.write(strSub)
+##outfile.write(storeIt)
+storeIt
+
