da2f3ad7655dd4a4cdea04ef721a61e493ee4d05
GildaGriffon <zxnoregretzxz@hotmail.co.uk>
2015-08-31 22:10:22 +0100

    Changed UA and am implementing some support for pickle.

--- RandomSOTD.py
+++ RandomSOTD.py
@@ -1,5 +1,5 @@
 import praw, pprint, pickle
-user_agent = "Gilda" ##input("Please input your desired user agent to display to reddit: ")
+user_agent = "/u/Gilda_Griffon's RSOTD Tool" ##input("Please input your desired user agent to display to reddit: ")
 r = praw.Reddit(user_agent=user_agent)
 nsfwCheck = input("Allow NSFW subs to be displayed?(True or False): ")
 subreddit = r.get_random_subreddit(nsfw=nsfwCheck)
@@ -8,6 +8,4 @@ strSub = str(subreddit)
 storeIt
 outfile = open('RSOTD.txt','w')
 outfile.write(strSub)
-##outfile.write(storeIt)
-storeIt
 
